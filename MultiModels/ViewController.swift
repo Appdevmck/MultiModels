4337c449d376110e964499be10fe3b712c2b8327
Lachlan McKerrow <appdev@mckerrow.com>
2020-08-19 14:51:12 +1000

    Initial Commit

---
+++ MultiModels/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiModels
+//
+//  Created by Lachlan McKerrow on 19/8/20.
+//  Copyright © 2020 Lachlan McKerrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
